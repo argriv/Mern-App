1869d954fb0c460ee4e18625237c4b651f27b9ef
Ivanov Grigory <argrivfrozen@gmail.com>
2022-09-29 23:00:59 +0300

    test-mern-fullstack

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,19 +5,19 @@ import {
 	CNavbar,
 	CNavbarBrand,
 } from '@coreui/react'
-import { FaBeer } from 'react-icons/fa'
+import { FaBitcoin } from 'react-icons/fa'
 
 const NavBar = () => {
 	return (
 		<CNavbar colorScheme='light' className='bg-slate-200'>
 			<CContainer fluid>
 				<CNavbarBrand to='/'>
-					<FaBeer />
+					<FaBitcoin/>
 				</CNavbarBrand>
 				<div className='flex'>
 					<Link
 						to='project'
-						className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 no-underline'
+						className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 no-underline'
 					>
 						Project
 					</Link>
